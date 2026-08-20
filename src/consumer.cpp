a09b06d3295a7989ddf6e69fd9278c4090aad38f
Mitrr0 <132539805+AleksandrZagrebin@users.noreply.github.com>
2026-08-20 13:23:51 +0400

    Create consumer.cpp

---
+++ src/consumer.cpp
@@ -0,0 +1,36 @@
+#include "consumer.hpp"
+#include <iostream>
+#include <mutex>
+
+extern int NTasks;
+extern std::mutex TaskMut;
+extern std::vector<int> Consumed;
+extern std::mutex ConsMut;
+
+void consume(ts_stack<int>& Q, int tasks_to_consume, int consumer_id)
+{
+    int consumed_count = 0;
+    while (consumed_count < tasks_to_consume)
+    {
+        {
+            std::lock_guard<std::mutex> Lk{ TaskMut };
+            if (NTasks < 0 && Q.is_empty_and_done())
+                break;
+        }
+
+        int N;
+        bool Succ = Q.wait_and_pop(N);
+
+        if (Succ)
+        {
+            {
+                std::lock_guard<std::mutex> Lk{ ConsMut };
+                Consumed.push_back(N);
+                consumed_count++;
+            }
+#ifdef VISUALIZE
+            std::cout << "Consumer " << consumer_id << " popped: " << N << std::endl;
+#endif
+        }
+    }
+}
